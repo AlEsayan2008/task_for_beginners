d1619c394c7c7bb1e262dc6316b805fa265ed0c7
AlEsayan2008 <167086118+AlEsayan2008@users.noreply.github.com>
2024-05-28 18:14:30 +0400

    Hackerrank's task

---
+++ task2.py
@@ -0,0 +1,10 @@
+def solveMefirst(a, b):
+    return a + b
+
+a = int(input("\nEnter first number\n"))
+b = int(input("\nEnter second number\n"))
+
+result = solveMefirst(a, b)
+print("---------------------")
+print(f"Final result is {result}.")
+print("---------------------")
