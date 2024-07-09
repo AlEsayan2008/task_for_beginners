daacf34b3724781d5bae187232371b5f87268004
AlEsayan2008 <167086118+AlEsayan2008@users.noreply.github.com>
2024-07-09 13:53:23 +0400

    Create task31.py

---
+++ task31.py
@@ -0,0 +1,30 @@
+Unique_n1=""
+Unique_n2=""
+Unique_n3=""
+Unique_n4=""
+Unique_n5=""
+count=0
+name=input()
+Unique_n1=name
+count+=1
+print(count)
+name=input()
+if name != Unique_n1:
+    Unique_n2=name
+    count+=1
+print(count)
+name=input()
+if name !=Unique_n1 and name != Unique_n2:
+    Unique_n3=name
+    count+=1
+print(count)
+name=input()
+if name !=Unique_n1 and name != Unique_n2 and name != Unique_n3:
+    Unique_n4=name
+    count+=1
+print(count)
+name=input()
+if name !=Unique_n1 and name != Unique_n2 and name != Unique_n3 and name!=Unique_n4:
+    Unique_n5=name
+    count+=1
+print(count)
