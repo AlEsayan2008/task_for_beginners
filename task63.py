d85db1ffd7c76a99c34c7561a42c5cd87509be90
AlEsayan2008 <167086118+AlEsayan2008@users.noreply.github.com>
2024-07-09 14:19:07 +0400

    Create task63.py

---
+++ task63.py
@@ -0,0 +1,11 @@
+x0 = int(input())
+x = int(input())
+y0 = int(input())
+y = int(input())
+t1 = x0 // x
+t2 = y0 // y
+
+if t1 > t2:
+    print("You will win")
+else:
+    print("There is a danger of losing")
