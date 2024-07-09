7984ecd2b924cbc3d7b25e7eaf1da090aef5ddef
AlEsayan2008 <167086118+AlEsayan2008@users.noreply.github.com>
2024-07-09 14:14:57 +0400

    Create task57.py

---
+++ task57.py
@@ -0,0 +1,20 @@
+ls = [
+    int(input()),
+    int(input()),
+    int(input()),
+    int(input()),
+    int(input()),
+    int(input()),
+    ]
+els = []
+a = ls[0] + ls[1]
+b = ls[1] + ls[2]
+c = ls[2] + ls[3]
+d = ls[3] + ls[4]
+e = ls[4] + ls[5]
+els.append(a)
+els.append(b)
+els.append(c)
+els.append(d)
+els.append(e)
+print(els)
