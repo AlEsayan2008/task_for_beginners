4c119e546a8ad642aa51bf03bb0534603977bf7d
AlEsayan2008 <167086118+AlEsayan2008@users.noreply.github.com>
2024-07-09 14:11:29 +0400

    Create task52.py

---
+++ task52.py
@@ -0,0 +1,14 @@
+ls = [
+    int(input()),
+    input(),
+    int(input()),
+    input(),
+    int(input()),
+    input(),
+    int(input()),
+    input()
+]
+
+sum_of_num = ls[0] + ls[2] + ls[4] + ls[6]
+sum_of_str = ls[1] + " " + ls[3] + " " + ls[5] + " " + ls[7]
+print(f"{sum_of_num}\n{sum_of_str}")
