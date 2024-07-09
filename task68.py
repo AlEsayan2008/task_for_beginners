000db862e79642fe229f0831dee6f86d9afd0097
AlEsayan2008 <167086118+AlEsayan2008@users.noreply.github.com>
2024-07-09 14:23:05 +0400

    Create task68.py

---
+++ task68.py
@@ -0,0 +1,10 @@
+n = int(input())
+phrases = []
+
+for _ in range(n):
+    phrase = input()
+    phrases.append(phrase)
+
+for i in range(n - 1):
+    print(phrases[i])
+print("I'LL WATCH YOUTUBE AND MEMES")
