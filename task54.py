303bf3fe4a528422791600aaa0d286948d51b506
AlEsayan2008 <167086118+AlEsayan2008@users.noreply.github.com>
2024-07-09 14:12:56 +0400

    Create task54.py

---
+++ task54.py
@@ -0,0 +1,19 @@
+ls = []
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+f = int(input())
+g = int(input())
+
+ls.append(a**2)
+ls.append(b**2)
+ls.append(c**2)
+ls.append(d**2)
+ls.append(e**2)
+ls.append(f**2)
+ls.append(g**2)
+
+print(ls)
