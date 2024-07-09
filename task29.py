ac7bd695c1b54b7432ed6ab2b3a535fdf4c9738e
AlEsayan2008 <167086118+AlEsayan2008@users.noreply.github.com>
2024-07-09 13:51:38 +0400

    Create task29.py

---
+++ task29.py
@@ -0,0 +1,19 @@
+a=input()
+a1=int(input())
+b=input()
+b1=int(input())
+c=input()
+c1=int(input())
+d=input()
+d1=int(input())
+e=input()
+e1=int(input())
+print('TOTAL:', a1)
+a1 += b1
+print('TOTAL:', a1)
+a1 += c1
+print('TOTAL:', a1)
+a1 += d1
+print('TOTAL:', a1)
+a1 += e1
+print('TOTAL:', a1)
