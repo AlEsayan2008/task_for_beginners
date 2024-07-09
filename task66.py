28b817833c7994f27910df9b953f929711a25bdb
AlEsayan2008 <167086118+AlEsayan2008@users.noreply.github.com>
2024-07-09 14:21:49 +0400

    Create task66.py

---
+++ task66.py
@@ -0,0 +1,10 @@
+months = [
+	'January', 'February',
+	'March', 'April', 'May',
+	'June', 'July', 'August',
+	'September', 'October', 'November',
+	'December'
+]
+
+for i in range(5):
+    print(months[i])
